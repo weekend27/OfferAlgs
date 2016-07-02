a061f2149219cfce8918493a38577ea75e626721
brilliantyy <weekend27@163.com>
2016-07-03 01:07:36 +0800

    add solutions

---
+++ src/java/ReverseList.java
@@ -0,0 +1,31 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+
+        // corner case
+        if (head == null) {
+            return null;
+        }
+
+        ListNode curr = head;
+        ListNode next = null;
+        ListNode prev = null;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
